8aae531a899776ad8d064dab21212f623a20454b
Karen Kalashyan <mb@on-i.ru>
2013-12-28 14:39:38 +0200

    The Plugin

---
+++ redactor-plugins/fontsize.js
@@ -0,0 +1,27 @@
+if (!RedactorPlugins) var RedactorPlugins = {};
+
+RedactorPlugins.fontsize = {
+	init: function()
+	{
+		var fonts = [10, 12, 14, 18, 22, 28, 36, 42, 60];
+		var that = this;
+		var dropdown = {};
+
+		$.each(fonts, function(i, s)
+		{
+			dropdown['s' + i] = { title: s + 'px', callback: function() { that.setFontsize(s); } };
+		});
+
+		dropdown['remove'] = { title: 'Remove font size', callback: function() { that.resetFontsize(); } };
+
+		this.buttonAdd( 'fontsize', 'Change font size', false, dropdown);
+	},
+	setFontsize: function(size)
+	{
+		this.inlineSetStyle('font-size', size + 'px');
+	},
+	resetFontsize: function()
+	{
+		this.inlineRemoveStyle('font-size');
+	}
+};
